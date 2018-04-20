d8caefdaca9e4d8a01c64069b3a9fc8b49f51cd3
miao <miao@qq.com>
2018-04-20 10:22:47 +0800

    lala

---
+++ hellow.py
@@ -0,0 +1,12 @@
+﻿#!\Users\Administrator.PC-201610151452\Desktop\python
+# -*- coding: utf-8 -*-
+print('hellow '*3)
+ 
+print('hellow '*3)
+ 
+print('hellow '*3)
+a=3
+b=3
+c=a+b
+print ('hellow '*c)
+print(' 啦啦啦')
